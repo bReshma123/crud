200306ab798733d50d207d7b0c57b1273636de31
Reshma <reshmabuduri@gmail.com>
2024-02-12 23:53:48 +0530

    created add student table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateComponent} from "./create/create.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'create', component:CreateComponent}
+  // {path:'aboutus', component:AboutusComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
