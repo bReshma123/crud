56f543bf9df5bafdb2919e50de16d67d7ea04313
haarun <haarunrasheed786@gmail.com>
2024-02-13 09:58:50 +0530

    created login interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateComponent} from "./create/create.component";
+import {LoginComponent} from "./login/login.component";
 
 const routes: Routes = [
+  {path: 'login',component:LoginComponent},
   {path:'create', component:CreateComponent}
   // {path:'aboutus', component:AboutusComponent},
 ];
